02e98f9357900a5cf38cb894a7b4a8046f62bc59
rainy-jain <jainrainy@gmail.com>
2022-10-29 19:14:20 +0530

    removing any access keys or reference of prod/uat env services

--- Microservices-Dashboard/try.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var Config = require("./Configs.json");
-var ConfigParams = JSON.parse(fs.readFileSync(__dirname +'/Configs.json').toString());
-console.log(Config)
-console.log(ConfigParams)
